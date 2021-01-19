ed00025253821488dc8270ee0cdb771b025b934c
Rizki0001 <76814708+Rizki0001@users.noreply.github.com>
2021-01-19 15:00:16 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [0815-1586-0089]
-│ • Telkomsel: [0813-5730-2007]
-│ • Smartfren: [0882-3543-5804]
+│ • Pulsa [0813-1599-5628]
+│ • Telkomsel: [0813-1599-5628]
+│ • Gopay: [0813-1599-5628]
 │ 「 Chat OWNER 」
-│ > Ingin donasi? Wa.me/6288235435804
+│ > Ingin donasi? Wa.me/6281315995628
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
